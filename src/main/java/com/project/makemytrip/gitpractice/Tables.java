8b7b43cfe4d15d50a1a72186646fcf053e704ae8
MaheshNalkande <149159615+MaheshNalkande@users.noreply.github.com>
2024-12-13 00:29:39 +0530

    Update Tables.java

--- src/main/java/com/project/makemytrip/gitpractice/Tables.java
+++ src/main/java/com/project/makemytrip/gitpractice/Tables.java
@@ -3,7 +3,7 @@ package com.project.makemytrip.gitpractice;
 public class Tables {
 
 	public static void main(String[] args) {
-		int i = 2;
+		int i = 4;
 		for (int j = 1; j <=10; j++) {
 			System.out.println(j*i);
 		}
